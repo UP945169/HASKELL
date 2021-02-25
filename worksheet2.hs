9c6fe77d7b2848b058041261a8b565f50e1b45c4
Susruth Puppala <up945169@myport.ac.uk>
2021-02-25 17:28:56 +0000

    Added new file

---
+++ worksheet2.hs
@@ -0,0 +1,18 @@
+sign :: Int -> Int
+sign x1
+    | x1 < 0 = -1
+    | x1 > 0 = 1
+    | otherwise = 0
+
+howManyEqual :: Int -> Int -> Int -> Int
+howManyEqual x1 x2 x3
+    | x1 == x2 && x1 == x3 = 2
+    | x1 == x2 && x1 == x3 && x2 == x3 = 3
+    | otherwise = 0
+
+--Fares start at £2.20; 50p is added for each kilometre
+--covered for the first 10 kilometres; 
+--and 30p is added foreach additional kilometre.
+
+taxiFare :: Int -> Float
+taxiFare 
